e3cfdcebed5b023a63db39bc649793dd0468e188
fawind <windheuser.fabian@gmail.com>
2018-12-12 17:34:58 +0100

    Store benchmarks to file

--- result-service/app/Main.hs
+++ result-service/app/Main.hs
@@ -4,7 +4,13 @@
 module Main where
 
 import Data.Monoid ((<>))
-import Data.Aeson (FromJSON, ToJSON)
+import Data.Maybe
+import Control.Monad
+import Control.Monad.Trans
+import Data.Aeson (FromJSON, ToJSON, encode, decode)
+import qualified Data.ByteString.Lazy as B
+import qualified Data.ByteString.Lazy.Char8 as Char8
+import qualified Data.ByteString.Internal as BS
 import GHC.Generics
 import Web.Scotty
 import Network.HTTP.Types (status200)
@@ -13,23 +19,31 @@ data Benchmark = Benchmark { name :: String, start :: String, end :: String } de
 instance ToJSON Benchmark
 instance FromJSON Benchmark
 
-benchmarkA :: Benchmark
-benchmarkA = Benchmark { name = "Benchmark A", start = "1", end = "2" }
-benchmarkB :: Benchmark
-benchmarkB = Benchmark { name = "Benchmark B", start = "1", end = "2" }
+benchmarksFile :: FilePath
+benchmarksFile = "benchmarks.log"
 
-allBenchmarks :: [Benchmark]
-allBenchmarks = [benchmarkA, benchmarkB]
+saveBenchmark :: Benchmark -> IO ()
+saveBenchmark benchmark = do
+  B.appendFile benchmarksFile $ B.snoc (encode benchmark) (BS.c2w '\n')
+
+loadBenchmarks :: IO Char8.ByteString
+loadBenchmarks = do B.readFile benchmarksFile
+
+decodeBenchmark :: [Char8.ByteString] -> [Benchmark]
+decodeBenchmark input = catMaybes (map decode input)
 
 getBenchmarks :: ActionM ()
 getBenchmarks = do
-  json allBenchmarks
+  content <- liftIO loadBenchmarks
+  let entries = Char8.lines content
+  json $ decodeBenchmark entries
 
 addBenchmark :: ActionM ()
 addBenchmark = do
   name <- param "name" :: ActionM String
   start <- param "start" :: ActionM String
   end <- param "end" :: ActionM String
+  liftIO $ saveBenchmark Benchmark { name = name, start = start, end = end }
   status status200
 
 routes :: ScottyM ()
