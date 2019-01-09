ef09907f690e2935f63880100a5c2e8df521f69d
Fabian Paul <fabian.paul@student.hpi.de>
2019-01-09 18:04:40 +0100

    Fix ps exporter install script

--- setup/install_ps_exporter.sh
+++ setup/install_ps_exporter.sh
@@ -3,18 +3,18 @@ set -e
 id -u node_exporter &>/dev/null || useradd --no-create-home --shell /bin/false node_exporter
 
 # install go
-add-apt-repository ppa:longsleep/golang-backports
+add-apt-repository -y ppa:longsleep/golang-backports
 apt-get update
-apt-get install golang-go
+apt-get install -y golang-go
 
 # Download go bin
 go get github.com/hpi-power-rangers/ps-exporter
-cp ~/go/bin/ps-exporter /usr/local/bin
+cp ~/go/bin/ps-exporter /usr/local/bin/ps_exporter
 
 # Service
-mkdir /usr/local/ps_exporter
-cp .ps_exporter/ps_exporter.sh /usr/local/ps_exporter
-cp ./systemd/ps_exporter.service /etc/systemd/sytem
+mkdir -p /usr/local/ps_exporter
+cp ./ps_exporter/ps_exporter.sh /usr/local/ps_exporter
+cp ./systemd/ps_exporter.service /etc/systemd/system/
 
 # Start service
 systemctl daemon-reload
@@ -22,4 +22,3 @@ systemctl start ps_exporter
 
 # Log service status
 systemctl status ps_exporter
-
--- setup/ps_exporter/ps_exporter.sh
+++ setup/ps_exporter/ps_exporter.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/local/bin/ps_exporter --host 1.0.0.0 --port 9097
+/usr/local/bin/ps_exporter --host 0.0.0.0 --port 9097
