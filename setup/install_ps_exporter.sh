8a796524de07c8a57823818dc8db2fbbc168d456
Fabian Paul <fabian.paul@student.hpi.de>
2019-01-16 14:35:12 +0100

    Fix permission issues for ldap and ps exporter

--- setup/install_ps_exporter.sh
+++ setup/install_ps_exporter.sh
@@ -5,22 +5,25 @@ set -e
 # Create user
 id -u node_exporter &>/dev/null || useradd --no-create-home --shell /bin/false node_exporter
 
+apt-get update
+
 # Required for add-apt-repository
 apt-get install -y software-properties-common
 
 # install go
 add-apt-repository -y ppa:longsleep/golang-backports
-apt-get update
 apt-get install -y golang-go
-
-# Download go bin
-go get github.com/hpi-power-rangers/ps-exporter
-cp ~/go/bin/ps-exporter /usr/local/bin/ps_exporter
 
 # Service
 mkdir -p /usr/local/ps_exporter
 cp ./ps_exporter/ps_exporter.sh /usr/local/ps_exporter
 cp ./systemd/ps_exporter.service /etc/systemd/system/
+
+# Download go bin
+cd /tmp
+export GOPATH=/tmp
+go get github.com/hpi-power-rangers/ps-exporter
+cp /tmp/bin/ps-exporter /usr/local/bin/ps_exporter
 
 # Start service
 systemctl daemon-reload
