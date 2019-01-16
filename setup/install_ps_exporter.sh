5dc2c309ff1f302e23ad55136dae05c3e6cc0513
Fabian Paul <fabian.paul@student.hpi.de>
2019-01-16 16:35:43 +0100

    Use release for ps exporter

--- setup/install_ps_exporter.sh
+++ setup/install_ps_exporter.sh
@@ -5,19 +5,13 @@ set -e
 # Create user
 id -u node_exporter &>/dev/null || useradd --no-create-home --shell /bin/false node_exporter
 
-apt-get update
-
-# Required for add-apt-repository
-apt-get install -y software-properties-common
-
-# install go
-add-apt-repository -y ppa:longsleep/golang-backports
-apt-get install -y golang-go
-
 if (systemctl -q is-active ps_exporter)
     then
     systemctl stop ps_exporter
 fi
+
+apt-get update
+apt-get install curl
 
 # Service
 mkdir -p /usr/local/ps_exporter
@@ -26,9 +20,9 @@ cp ./systemd/ps_exporter.service /etc/systemd/system/
 
 # Download go bin
 cd /tmp
-export GOPATH=/tmp
-go get github.com/hpi-power-rangers/ps-exporter
-cp /tmp/bin/ps-exporter /usr/local/bin/ps_exporter
+curl -L https://github.com/hpi-power-rangers/ps-exporter/releases/download/v0.0.1/ps-exporter_0.0.1_Linux_amd64.tar.gz > ps-exporter.tar.gz
+tar -xzf ps-exporter.tar.gz
+cp ./ps-exporter /usr/local/bin/ps_exporter
 
 # Start service
 systemctl daemon-reload
