b4598c7b94bd9c7e302abbee85d37e5bfddadf5f
Fabian Paul <fabian.paul@student.hpi.de>
2019-01-30 14:01:45 +0100

    Register worker on service start

--- setup/worker/broadcast.sh
+++
@@ -1,1 +0,0 @@
-nmap -oG - -p7249 "$(hostname -I)"/24 | grep open | awk '{print $2}' | xargs -I '{}' sh -c 'echo "{} $(curl -s http://{}:7249/broadcast)"' | grep "exporter-registry" | awk '{print $1}'
--- setup/worker/packages/nodeexporter-1.0/usr/local/node_exporter/node-exporter.sh
+++ setup/worker/packages/nodeexporter-1.0/usr/local/node_exporter/node-exporter.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 export PORT=7249
-LABEL="ps_exporter"
-EXPORTER_PORT=9290
+LABEL="node_exporter"
+EXPORTER_PORT=9100
 masterIp=$(nmap -oG - -p$PORT "$(hostname -I)"/24 | grep open | awk '{print $2}' | xargs -I '{}' sh -c 'echo "{} $(curl -s http://{}:$PORT/broadcast)"' | grep "exporter-registry" | awk '{print $1}')
 echo $masterIp
 curl "${masterIp}:${PORT}/register?label=${LABEL}&port=${EXPORTER_PORT}"
-/usr/local/ipmi_exporter/ipmi_exporter -config.file /usr/local/ipmi_exporter/ipmi.yml
-
+/usr/local/bin/node_exporter
