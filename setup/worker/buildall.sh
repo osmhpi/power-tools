f849469f7f316853b6377c464686c7033bbdfef3
fawind <windheuser.fabian@gmail.com>
2019-02-13 17:24:52 +0100

    Set version in build script

--- setup/worker/buildall.sh
+++ setup/worker/buildall.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+VERSION="1.0"
+
 dpkg-deb --build psexporter
 dpkg-deb --build nodeexporter
 dpkg-deb --build ipmiexporter
+
+mv psexporter.deb "psexporter-$VERSION.deb"
+mv nodeexporter.deb "nodeexporter-$VERSION.deb"
+mv ipmiexporter.deb "ipmiexporter-$VERSION.deb"
