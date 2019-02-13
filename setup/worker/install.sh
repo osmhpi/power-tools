83fdf875b92a91ae8c05864143f022b749c301e9
fawind <windheuser.fabian@gmail.com>
2019-02-13 17:34:54 +0100

    Update curl install script

--- setup/worker/install.sh
+++ setup/worker/install.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-curl -LOs https://github.com/hpi-power-rangers/power-tools/releases/download/0.0.1/ipmiexporter.deb
+curl -LOs https://github.com/hpi-power-rangers/power-tools/releases/download/0.0.1/ipmiexporter-1.0.deb
 
-curl -LOs https://github.com/hpi-power-rangers/power-tools/releases/download/0.0.1/nodeexporter.deb
+curl -LOs https://github.com/hpi-power-rangers/power-tools/releases/download/0.0.1/nodeexporter-1.0.deb
 
-curl -LOs https://github.com/hpi-power-rangers/power-tools/releases/download/0.0.1/psexporter.deb
+curl -LOs https://github.com/hpi-power-rangers/power-tools/releases/download/0.0.1/psexporter-1.0.deb
 
 apt remove -y psexporter
 apt remove -y nodeexporter
 apt remove -y ipmiexporter
 
-apt install -y -f ./psexporter.deb
-apt install -y -f ./nodeexporter.deb
-apt install -y -f ./ipmiexporter.deb
+apt install -y -f ./psexporter-1.0.deb
+apt install -y -f ./nodeexporter-1.0.deb
+apt install -y -f ./ipmiexporter-1.0.deb
