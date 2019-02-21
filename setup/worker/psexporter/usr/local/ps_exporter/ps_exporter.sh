3870bd3871d93097aedcb0e08e1d1d77ff0336d0
Bernhard Rabe <bernhard.rabe@gmx.net>
2019-02-21 01:20:54 +0100

    Prevent error message

--- setup/worker/psexporter/usr/local/ps_exporter/ps_exporter.sh
+++ setup/worker/psexporter/usr/local/ps_exporter/ps_exporter.sh
@@ -7,5 +7,5 @@ PORT=9097
 
 nmap -sT -oG - -p$REGISTRY_PORT "$(hostname -I)"/24 | grep open | awk '{print $2}' | xargs -I '{}' curl -s "http://{}:$REGISTRY_PORT/register?label=$LABEL&port=$PORT"
 
-source /usr/local/ps_exporter/ps_exporter.env
+[ -f /usr/local/ps_exporter/ps_exporter.env ] && . /usr/local/ps_exporter/ps_exporter.env
 /usr/local/ps_exporter/ps_exporter --host ${HOST} --port ${PORT}
