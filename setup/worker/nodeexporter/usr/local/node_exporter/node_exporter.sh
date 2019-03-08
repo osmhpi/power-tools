e0c48fb9ed743396468cb493d966e7b5137d03ec
Bernhard Rabe <bernhard.rabe@gmx.net>
2019-03-08 10:53:11 +0100

    Simple fix to prevent error message if env file is not available (#1)
    * Prevent error message
    
    * Prevent error message
    
    * Prevent error message

--- setup/worker/nodeexporter/usr/local/node_exporter/node_exporter.sh
+++ setup/worker/nodeexporter/usr/local/node_exporter/node_exporter.sh
@@ -6,5 +6,5 @@ PORT=9100
 
 nmap -sT -oG - -p$REGISTRY_PORT "$(hostname -I)"/24 | grep open | awk '{print $2}' | xargs -I '{}' curl -s "http://{}:$REGISTRY_PORT/register?label=$LABEL&port=$PORT"
 
-source /usr/local/node_exporter/node_exporter.env
+[ -f /usr/local/node_exporter/node_exporter.env ] && . /usr/local/node_exporter/node_exporter.env
 /usr/local/bin/node_exporter --web.listen-address=":${PORT}"
